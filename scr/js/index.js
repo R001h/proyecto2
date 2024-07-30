deee8b89888e22db2154378deab3c0ca36ab1df7
R001h <rortega@fwdcostarica.com>
2024-07-30 10:42:38 -0600

    cambios

--- scr/js/index.js
+++ scr/js/index.js
@@ -1,30 +1,31 @@
 const username = document.getElementById("username")
 const psw = document.getElementById("psw")
 const email= document.getElementById("email")
-const sign_up=document.getElementById("signUp")
+const sign_up=document.getElementById("sign_up")
   ///guardar en el storage(Json para entrada y salida string-entero)///
 const listusername= JSON.parse(localStorage.getItem("listusername")||[])
 
 sign_up.addEventListener("click",function () {
+
+    console.log(2);
     const  userDatabase = username.value;
     const  pswDatabase = psw.value;
-
-let userknow = false
+   
+    let userknow = false
 
     for (let index = 0; index < listusername.length; index++) {
-        const user = listusername[i]
+        const user = listusername[index]
             //consulta lo guardado con ingresado////
         if (user.username === userDatabase && user.psw === pswDatabase) {
             userknow = true;
             break; ///sale de ciclo//  
         }
-        
-    }
-    if (userknow){
-        window.location="portaljob.html";
-    } else{
-        alert("User name or password incorrect")
-    }
+        }
+        if (userknow){
+            window.location="portaljob.html";
+        } else{
+            alert("User name or password incorrect")
+        }
 
        
 })

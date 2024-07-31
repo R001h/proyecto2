85794fe573f65be35b7d9ffcc7faff2635671eb3
R001h <rortega@fwdcostarica.com>
2024-07-31 11:09:41 -0600

    cambios update

--- scr/js/portal.js
+++ scr/js/portal.js
@@ -3,23 +3,32 @@ const addJ = document.getElementById("add1");
 const containerTask = document.getElementById("containerTask");
 const prioritySelect = document.getElementById("prioritySelect");
 const typeSelect = document.getElementById("typeSelect");
-
-
-const eventTitle = document.getElementById("eventTitle");
 const eventDate = document.getElementById("eventDate");
-const addEventBtn = document.getElementById("addEventBtn");
-const eventsContainer = document.getElementById("eventsContainer");
+const dateLabel = document.getElementById("dateLabel");
 
+typeSelect.addEventListener("change",function(){
+    if (typeSelect.value === "event"){
+        eventDate.style.display = " inline";
+        eventLabel.style.display = " inline";
+    } else {
+        eventDate.style.display= "none";
+        eventLabel.style.display = "none";
+    }
+});
 
 addJ.addEventListener("click",function(){
-   const taskS = taskMain.value.trim; // calor campo text
-   const selectedType = typeSelect.value; // Obtiene (tarea o evento)
-   const selectedPriority = prioritySelect.value; // Obtieneprioridad seleccionada
+    const taskS = taskMain.value; // calor campo text
+    const selectedType = typeSelect.value; // Obtiene (tarea o evento)
+    const selectedPriority = prioritySelect.value; // Obtieneprioridad seleccionada
+    const eventDateValue = eventDate.value;
 
-    if(taskS ===""){
-        alert("please introduce task or event") // No campos vacios
+
+    if(taskS.trim ===""){
+        alert("please introduce task or event");// No campos vacios
         return;
     }
+
+    cons divHijo = document.createElement ("div")
 
         ptask.innerHTML=taskS
         btnDelete.innerHTML="delete"
